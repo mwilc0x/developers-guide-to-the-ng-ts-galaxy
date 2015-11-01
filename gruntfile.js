2b3ebca6c39d7a1f4580c4b80e6bdb2e12c05179
Mike Wilcox <mwilcox56@gmail.com>
2015-11-01 13:30:25 -0500

    added gruntfile

---
+++ gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  config();
+  tasks();
+
+  function config() {
+    var config = require('load-grunt-config')(grunt);
+    grunt.initConfig(config);
+  }
+
+  function tasks() {
+    grunt.registerTask('build', []);
+  }
+}
