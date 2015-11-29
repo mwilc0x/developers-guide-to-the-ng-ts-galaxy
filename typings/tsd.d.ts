226d16aadafe0d15dc546d310076750bfbf69874
Mike Wilcox <mwilcox56@gmail.com>
2015-11-29 16:57:15 -0500

    update typings for redux modules

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,5 +1,7 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="redux/redux.d.ts" />
+/// <reference path="ng-redux/ng-redux.d.ts"/>
 
 interface TodoItem {
     title: string;
