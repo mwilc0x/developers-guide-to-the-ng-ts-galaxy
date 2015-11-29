5a565e924e9e85492cbd9c8ce7e4ad9ee989ce39
Mike Wilcox <mwilcox56@gmail.com>
2015-11-29 17:12:14 -0500

    add reducer, hookup redux

--- src/components/App/App.module.ts
+++ src/components/App/App.module.ts
@@ -1,11 +1,13 @@
 import * as angular from 'angular';
+import {todos} from '../../reducers/index';
 import { combineReducers } from 'redux';
 import {App} from './App.controller';
 import 'ng-redux';
 
 const ngModule = angular.module('todomvc', ['ngRedux'])
 .config(($ngReduxProvider) => {
-  
+	let reducer = combineReducers(todos);
+  $ngReduxProvider.createStoreWith(reducer, []);
 })
 .controller('App', App);
 
