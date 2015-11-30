1d0c25afdbbd59209507f0d80932ec1e6552598e
Mike Wilcox <mwilcox56@gmail.com>
2015-11-30 00:17:21 -0500

    fix indentation

--- src/components/TodoFooter/TodoFooter.controller.ts
+++ src/components/TodoFooter/TodoFooter.controller.ts
@@ -1,15 +1,17 @@
 export class TodoFooterCtrl {
-	location: any;
-	todos: Array<TodoItem>;
-
-	static $inject = ['$scope', '$location'];
-	constructor(private $scope: ng.IScope,
-		private $location: ng.ILocationService) {
-		this.location = $location;
+  location: any;
+  todos: Array<TodoItem>;
+  
+  static $inject = ['$scope', '$location'];
+  constructor(private $scope: ng.IScope,
+    private $location: ng.ILocationService
+  ) {
+    this.location = $location;
+    
     $scope.$watch('footer.location', location => this.location = location);
-	}
-
-	clearDoneTodos() {
-  	this.todos = this.todos = this.todos.filter(todoItem => !todoItem.completed);
+  }
+  
+  clearDoneTodos() {
+    this.todos = this.todos = this.todos.filter(todoItem => !todoItem.completed);
   };
 }
