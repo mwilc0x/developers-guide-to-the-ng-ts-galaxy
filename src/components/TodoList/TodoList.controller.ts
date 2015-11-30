afb1f3fd99593294381d0e502711dfed74eb8ce6
Mike Wilcox <mwilcox56@gmail.com>
2015-11-30 00:13:37 -0500

    fix indentation

--- src/components/TodoList/TodoList.controller.ts
+++ src/components/TodoList/TodoList.controller.ts
@@ -1,35 +1,35 @@
 import * as TodoActions from '../../actions/todos';
 
 export class TodoListCtrl {
-	editedTodo: TodoItem;
-	todos: Array<TodoItem>;
-	deleteTodo: Function;
-	
-	static $inject = ['$scope', '$log', '$ngRedux'];
-	constructor(private $scope, private $log, private $ngRedux) {
-		let unsubscribe = $ngRedux.connect(
-			this.mapStateToThis.bind(this), 
-			TodoActions
-		)(this);
-		
+  editedTodo: TodoItem;
+  todos: Array<TodoItem>;
+  deleteTodo: Function;
+  
+  static $inject = ['$scope', '$log', '$ngRedux'];
+  constructor(private $scope, private $log, private $ngRedux) {
+    let unsubscribe = $ngRedux.connect(
+      this.mapStateToThis.bind(this), 
+      TodoActions
+    )(this);
+    
     $scope.$on('$destroy', unsubscribe);
-	}
+  }
 	
-	editTodo(todoItem: TodoItem) { }
+  editTodo(todoItem: TodoItem) { }
 	
-	removeTodo(todo: TodoItem) {
-		this.deleteTodo(todo.id);
-	}
-	
-	doneEditing(todoItem: TodoItem) {
-		this.editedTodo = null;
-		todoItem.title = todoItem.title.trim();
-		if (!todoItem.title) {
-			this.removeTodo(todoItem);
-		}
-	}
-	
-	mapStateToThis(state) {
+  removeTodo(todo: TodoItem) {
+    this.deleteTodo(todo.id);
+  }
+  
+  doneEditing(todoItem: TodoItem) {
+    this.editedTodo = null;
+    todoItem.title = todoItem.title.trim();
+    
+    if (!todoItem.title) {
+      this.removeTodo(todoItem);
+    }
+  }
+  mapStateToThis(state) {
     return {};
   }
 }
