d867b18407e4f004d19dfe80efa7632bb9d42ef2
Mike Wilcox <mwilcox56@gmail.com>
2015-11-29 21:59:05 -0500

    remove js files

--- src/components/App/App.module.js
+++
@@ -1,8 +0,0 @@
-var angular = require('angular');
-var App_controller_1 = require('./App.controller');
-require('ng-redux');
-var ngModule = angular.module('todomvc', ['ngRedux'])
-    .controller('App', App_controller_1.App);
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = ngModule;
-//# sourceMappingURL=App.module.js.map
