b60c0cf1af81a801a8dc3753201c9ff5e39faeb3
Mike Wilcox <mwilcox56@gmail.com>
2015-11-30 00:16:04 -0500

    fix indentation

--- src/components/TodoHeader/Header.controller.ts
+++ src/components/TodoHeader/Header.controller.ts
@@ -1,30 +1,30 @@
 import * as TodoActions from '../../actions/todos';
 
 export class HeaderCtrl {
-	newTodo: string = '';
-	addTodo: Function;
-	
-	static $inject = [
-		'$scope',  
-		'$ngRedux'
-	];
-	constructor(private $scope: ng.IScope,
-		private $ngRedux: ngRedux.INgRedux) {
-			
-		let unsubscribe = $ngRedux.connect(
-			this.mapStateToThis.bind(this), 
-			TodoActions
-		)(this);
-		
-		$scope.$on('$destroy', unsubscribe);
-	}
-	
-	submit() {
-		this.addTodo(this.newTodo);
-		this.newTodo = '';
-	}
-	
-	mapStateToThis(state) {
+  newTodo: string = '';
+  addTodo: Function;
+  
+  static $inject = [
+    '$scope',
+    '$ngRedux'
+  ];
+  
+  constructor(private $scope: ng.IScope,
+    private $ngRedux: ngRedux.INgRedux) {
+      let unsubscribe = $ngRedux.connect(
+        this.mapStateToThis.bind(this),
+        TodoActions
+      )(this);
+      
+      $scope.$on('$destroy', unsubscribe);
+  }
+  
+  submit() {
+    this.addTodo(this.newTodo);
+    this.newTodo = '';
+  }
+  
+  mapStateToThis(state) {
     return {
       todos: state.todos
     };
